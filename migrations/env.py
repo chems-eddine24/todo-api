392e8bbecc5c50c9aac0df02769c46706e9457e1
chems-eddine24 <obitouchiwa109@gmail.com>
2025-11-18 17:31:31 +0100

    stop tracking .env

--- migrations/env.py
+++ migrations/env.py
@@ -1,19 +1,33 @@
 from logging.config import fileConfig
-
+import os
 from sqlalchemy import engine_from_config, pool
 from sqlalchemy import pool
 from alembic import context
 from app.db.core import *
+from dotenv import load_dotenv
+load_dotenv()
+
+db_url = os.getenv('DATABASE_URL')
 
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+db_url = os.getenv("DATABASE_URL")
+
+# Alembic MUST NOT use asyncpg → convert to sync driver
+if db_url and db_url.startswith("postgresql+asyncpg"):
+    sync_url = db_url.replace("postgresql+asyncpg", "postgresql")
+else:
+    sync_url = db_url
+
+config.set_main_option("sqlalchemy.url", sync_url)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
