4b04cbae4acbfb5ccd0abaa2957bcb0e4c4f7758
chems-eddine24 <obitouchiwa109@gmail.com>
2025-12-26 01:42:43 +0100

    delete endpoints from main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,7 @@
 
-from fastapi import FastAPI, HTTPException, Depends, APIRouter, status
-from requests import Session
-from app.schemas.schemas_task import *
-from app.schemas.schemas_user import *
-from typing import Optional
-from app.models.db_task import *
-from app.core.db_core import *
-from app.models.db_user import *
-from app.models.db_task import *
+from fastapi import FastAPI
 from app.endpoints import tasks, users
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
-from app.endpoints.auth import get_current_user
-from fastapi.security import OAuth2PasswordRequestForm
-from repository.tasks_repository import TasksRepository
+
 
 app = FastAPI(title="Todo API")
 app.include_router(tasks.router)
