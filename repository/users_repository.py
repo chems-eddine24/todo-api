f773801f72d9831541705e0bcfe3b9162e8ffc67
chems-eddine24 <obitouchiwa109@gmail.com>
2025-12-31 12:21:29 +0100

    get user by id and by email

--- repository/users_repository.py
+++ repository/users_repository.py
@@ -1,7 +1,7 @@
-from typing import  Optional
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from app.models.db_user import User
+from app.schemas.schemas_user import UserR
 
 
 
@@ -9,17 +9,22 @@ class UsersRepository:
     def __init__(self, db: AsyncSession):
         self.db = db
 
-    async def register_user(self, email: str, password_hash: str) -> User:
-        query = await self.db.execute(select(User).where(User.email == email))
+    async def register_user(self, email: str, password_hash: str) -> UserR:
         user = User(
             email=email,
             password_hash=password_hash
         )
         
         self.db.add(user)
+        await self.db.commit()
+        await self.db.refresh(user)
         return user
     
-    async def login_user(self, user_id: str):
-        user = await self.db.execute(select(User).where(User.id == user_id))
-        return user
+    async def get_user_by_email(self, email: str) -> UserR :
+        user = await self.db.execute(select(User).where(User.email == email))
+        return user.scalars().first()
+    
+    async def get_user_by_id(self, id: str):
+        user = await self.db.execute(select(User).where(User.id == id))
+        return user.scalars().first()
 
