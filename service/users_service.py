1a5425159e86e427f912cc8c6f8ab84e608dc671
chems-eddine24 <obitouchiwa109@gmail.com>
2025-12-31 12:22:42 +0100

    updated user service

--- service/users_service.py
+++ service/users_service.py
@@ -1,21 +1,24 @@
-from typing import Optional
-from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.error_handler import UserHandler
 from repository.users_repository import UsersRepository
-from app.models.db_user import User
-
+from app.schemas.schemas_user import UserR
+from service.auth_service import AuthService
 
 
 class UsersService:
-    def __init__(self, db: AsyncSession):
+    def __init__(self, db):
         self.db = db
-        self.repository = UsersRepository(db)
 
-    async def register_user(self, email: str, password_hash: str) -> User:
-        user = UsersRepository(db=self.db).register_user(email=email, password_hash=password_hash)
+    async def register_user(self, email: str, password_hash: str) -> UserR:
+        if await UsersRepository(self.db).get_user_by_email(email):
+            return UserHandler.existing_user_error()
+        if email is None or email == "":
+            return UserHandler.email_error()
+        user = await UsersRepository(db=self.db).register_user(email=email, password_hash=password_hash)
         return user
     
-    async def login_user(self, user_id: str):
-        user = UsersRepository(db=self.db).login_user(user_id=user_id)
+    async def login_user(self, email: str, password: str):
+        user = await AuthService(db=self.db).authenticate_user(email, password)
         return user
+
 
 
