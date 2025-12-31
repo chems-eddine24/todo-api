484d097e98faaeca31820cc20f5277f8ea7a302a
chems-eddine24 <obitouchiwa109@gmail.com>
2025-12-31 12:19:58 +0100

    updated main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 
 from fastapi import FastAPI
-from app.endpoints import tasks, users
+from app.endpoints import auth, tasks
 
 
 app = FastAPI(title="Todo API")
 app.include_router(tasks.router)
-app.include_router(users.router)
+app.include_router(auth.router)
 
 
