4b17062295fe7afaa43d894c3819f756ca063660
chems-eddine24 <obitouchiwa109@gmail.com>
2026-01-05 10:19:07 +0100

    security fucntions for authentification

---
+++ app/core/security.py
@@ -0,0 +1,115 @@
+from fastapi import Depends, HTTPException, status, Request
+from datetime import datetime, timedelta, timezone
+from app.core.db_core import get_db
+from uuid import UUID
+from sqlalchemy.ext.asyncio import AsyncSession
+from jose import jwt, JWTError
+from jwt.exceptions import InvalidTokenError, ExpiredSignatureError
+from app.repositories.users_repository import UsersRepository
+from app.repositories.base_repository import BaseRepository
+from app.core.config import settings
+
+
+
+
+
+def get_password_hash(password: str) -> str:
+        return settings.pwd_context.hash(password)
+def verify_password(plain_password: str, password_hash: str) -> bool:
+        return settings.pwd_context.verify(plain_password, password_hash)
+
+def authenticate_user(email: str, password: str, db: AsyncSession = Depends(get_db)):
+    user = UsersRepository.get_user_by_email(email=email)
+    if not user or not verify_password(password, get_password_hash(password=password)):
+        return False
+    return user
+
+def create_access_token(user_id: str, expires_delta: timedelta | None = None):
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload = {"sub": str(user_id), "exp": expire, "type": "access"}
+    token = jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return token
+
+def create_refresh_token(subject: str, token_version: int = 0, expires_delta: timedelta = timedelta(days=7)) -> str:
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(days=7))
+    to_encode = {
+        'exp': int(expire.timestamp()),
+        'token_version': token_version,
+        'sub': str(subject),
+        'type': 'refresh'
+    }
+    refresh_token = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return refresh_token
+
+def verify_refresh_token(token: str):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        if payload.get("type") != "refresh":
+            raise JWTError("Invalid token type")
+        return payload
+    
+    except ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Refresh token has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid refresh token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate refresh token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        return payload
+    except ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+async def get_current_user(request: Request, db: AsyncSession = Depends(get_db)):
+    token = request.cookies.get("access_token")
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="token missing",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        user_id = payload.get("sub")
+        user = await UsersRepository(session=db).get_user_by_id(UUID(user_id))
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="User not found",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        return user
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
