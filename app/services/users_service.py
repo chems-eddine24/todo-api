2cc5c1c6ab95da003d2baa6bf5325944b41efb62
chems-eddine24 <obitouchiwa109@gmail.com>
2026-01-09 10:08:18 +0100

    added authentication services to user_service.py

--- app/services/users_service.py
+++ app/services/users_service.py
@@ -1,10 +1,10 @@
 from app.core.exceptions import AppError, ErrorCode
 from app.repositories.users_repository import UsersRepository
-from fastapi import status
+from fastapi import status, Response, Request, HTTPException
 from app.models.db_user import User
 from app.schemas.schemas_user import UserCreate
 from app.services.base_service import BaseService
-from app.core.security import get_password_hash, verify_password
+from app.core.security import (get_password_hash, verify_password, create_access_token, create_refresh_token, verify_access_token, verify_refresh_token)
 
 class UsersService(BaseService):
     def __init__(self, session):
@@ -36,3 +36,71 @@ class UsersService(BaseService):
         if not user or not verify_password(password, get_password_hash(password)):
             return False
         return user
+    async def login_for_access_token(self, response: Response, email, password):
+        user = await UsersService(self.session).login_user(email=email, password=password)
+        if not user:
+            raise AppError(
+                message='User not found with the given email and password, passwrod or email incorrect',
+                error_code=ErrorCode.USER_NOT_FOUND,
+                status_code=status.HTTP_400_BAD_REQUEST
+            )
+        user_id = user.id
+        access_token = create_access_token(user_id=str(user_id))
+        refresh_token = create_refresh_token(subject=str(user_id))
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            samesite="lax",
+            max_age=1800,
+            secure=False,
+            path='/'
+            )
+        response.set_cookie(
+            key="refresh_token",
+            value=refresh_token,
+            httponly=True,
+           samesite="lax",
+           max_age=7*24*60*60,
+           secure=False,
+           path='/'
+           )
+        return {'message':f"welcome {user.email} "}
+    
+    async def refresh_access_token(self, response: Response, request: Request):
+        refresh_token = request.cookies.get("refresh_token")
+        if not refresh_token:
+            raise HTTPException(
+                status_code=401,
+                detail="Refresh token missing",
+                )
+        payload = verify_refresh_token(refresh_token)
+        user_id = payload.get("sub")
+        new_access_token = create_access_token(user_id=user_id)
+        new_refresh_token = create_refresh_token(subject=user_id)
+
+        response.set_cookie(
+            key="access_token",
+            value=new_access_token,
+            httponly=True,
+            samesite="lax",
+            max_age=1800,
+            secure=False,
+            path='/'
+            )
+        response.set_cookie(
+            key="refresh_token",
+            value=new_refresh_token,
+            httponly=True,
+            samesite="lax",
+            max_age=7*24*60*60,
+            secure=False,
+            path='/'
+            )
+        return {'message': 'access token refreshed successfully'}
+    
+
+    async def logout(self, response: Response):
+        response.delete_cookie('access_token')
+        response.delete_cookie('refresh_token')
+        return {'message':'logged out'}
